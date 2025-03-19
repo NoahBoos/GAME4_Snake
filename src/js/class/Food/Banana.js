eee820a24ecdb479cedfe2b5085079e0463e574a
Noah Boos <nboosgamemaker@gmail.com>
2025-03-19 12:14:45 +0100

    Feature - Added Strawberry and Banana classes

---
+++ src/js/class/Food/Banana.js
@@ -0,0 +1,19 @@
+import {
+    Food
+} from "./Food.js"
+
+export class Banana extends Food {
+    static color = "#FFE01B";
+    static experience = 10;
+    static segmentsToAdd = 2;
+
+    constructor(context, terrain, coordinateX, coordinateY) {
+        super(
+            context,
+            terrain,
+            Banana.color,
+            Banana.experience,
+            Banana.segmentsToAdd
+        );
+    }
+}
---
+++ src/js/class/Food/Strawberry.js
@@ -0,0 +1,19 @@
+import {
+    Food
+} from "./Food.js";
+
+export class Strawberry extends Food {
+    static color = "#FF0000";
+    static experience = 5;
+    static segmentsToAdd = 1;
+
+    constructor(context, terrain) {
+        super(
+            context,
+            terrain,
+            Strawberry.color,
+            Strawberry.experience,
+            Strawberry.segmentsToAdd
+        );
+    }
+}
