a9cdff77339b903e9d3f4f0bae42f7e35b9068a2
Noah Boos <nboosgamemaker@gmail.com>
2025-03-20 19:45:28 +0100

    Code Review - JSDoc "Banana.js"

--- src/js/class/Food/Banana.js
+++ src/js/class/Food/Banana.js
@@ -2,21 +2,32 @@ import {
     Food
 } from "./Food.js"
 
-/**
- *
- */
 export class Banana extends Food {
-    static color = "#FFE01B";
-    static experience = 10;
-    static segmentsToAdd = 2;
-
     /**
-     *
-     * @param context
-     * @param terrain
+     * @type {string} color
+     * Couleur du fruit
+     */
+    static color = "#FFE01B";
+    /**
+     * @type {number} experience
+     * Somme des points d'expériences que le joueur doit gagner s'il mange le fruit.
+     */
+    static experience = 10;
+    /**
+     * @type {number} segmentsToAdd
+     * Nombre de segments à ajouter au serpent.
+     */
+    static segmentsToAdd = 2;
+    /**
+     * Le constructeur de la classe Banana.
+     * @param {CanvasRenderingContext2D} context - Contexte utilisé pour interagir sur le canvas.
+     * @param {Terrain} terrain - Instance de la classe Terrain.
      * @constructor
      */
     constructor(context, terrain) {
+        /**
+         * Appel au constructeur de la classe dont Banana hérite, c'est-à-dire "Food".
+         */
         super(
             context,
             terrain,
@@ -24,11 +35,13 @@ export class Banana extends Food {
             Banana.experience,
             Banana.segmentsToAdd
         );
-
+        /**
+         * Définit les coordonnées de l'aliment.
+         * Écrit les données de la cellule où se trouve l'aliment.
+         * Dessine l'aliment.
+         */
         this.DefineCoordinates();
         this.WriteFoodCell();
         this.DrawFood();
-
-        console.log(this);
     }
 }
