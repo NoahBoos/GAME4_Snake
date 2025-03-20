3875a9d6da366a6f13694dd4582db29283b94e43
Noah Boos <nboosgamemaker@gmail.com>
2025-03-20 20:00:27 +0100

    Code Review - JSDoc "global.js"

--- src/js/global.js
+++ src/js/global.js
@@ -1,9 +1,21 @@
-export const CELL_SIZE = 20;
-export let movementIndex = -1;
-
 /**
- *
- * @param newMovementIndex
+ * @type {number} CELL_SIZE
+ * La taille d'une cellule. Puisqu'une cellule est nécessairement carrée, on n'entrepose qu'une valeur.
+ */
+export const CELL_SIZE = 20;
+/**
+ * @type {number} movementIndex
+ * L'index de mouvement déterminant la direction du serpent.
+ * - -1 : Statique, le serpent ne bougera pas.
+ * - 0 : Le serpent ira de l'avant (en-haut).
+ * - 1 : Le serpent ira de l'arrière (en-bas).
+ * - 2 : Le serpent ira vers sa droite.
+ * - 3 : Le serpent ira vers sa gauche.
+ */
+export let movementIndex = -1;
+/**
+ * Met à jour l'index de mouvement.
+ * @param {number} newMovementIndex - L'index de mouvement déterminant la direction du serpent.
  */
 export function MovementIndex(newMovementIndex) {
     movementIndex = newMovementIndex;
