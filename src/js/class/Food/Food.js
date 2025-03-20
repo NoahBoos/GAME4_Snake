6e7bdcd71b3e7cdc6ffe2ae0c02d573c9755d025
Noah Boos <nboosgamemaker@gmail.com>
2025-03-20 19:56:12 +0100

    Code Review - JSDoc "Food.js"

--- src/js/class/Food/Food.js
+++ src/js/class/Food/Food.js
@@ -1,17 +1,14 @@
 import {GetRandomCoordinates} from "../../utils.js";
 import {CELL_SIZE} from "../../global.js";
 
-/**
- *
- */
 export class Food {
     /**
-     *
-     * @param context
-     * @param terrain
-     * @param color
-     * @param experience
-     * @param segmentsToAdd
+     * Le constructeur de la classe Food.
+     * @param {CanvasRenderingContext2D} context - Contexte utilisé pour interagir sur le canvas.
+     * @param {Terrain} terrain - Instance de la classe Terrain.
+     * @param {string} color - Couleur de la tête du serpent.
+     * @param {number} experience - Somme des points d'expériences que le joueur doit gagner s'il mange le fruit.
+     * @param {number} segmentsToAdd - Nombre de segments à ajouter au serpent.
      * @constructor
      */
     constructor(context, terrain, color, experience, segmentsToAdd) {
@@ -23,9 +20,8 @@ export class Food {
         this.experience = experience;
         this.segmentsToAdd = segmentsToAdd;
     }
-
     /**
-     *
+     * Dessine un fruit dans le canvas. Chaque fruit a une dimension de 20 pixels * 20 pixels.
      */
     DrawFood() {
         this.context.fillStyle = this.color;
@@ -33,18 +29,29 @@ export class Food {
     }
 
     /**
-     *
+     * Écrit les données d'une cellule de nourriture en appelant WriteTerrainCell().
+     * - isOccupied = true ;
+     * - type = "food" ;
+     * - object = this
      */
     WriteFoodCell() {
         this.terrain.WriteTerrainCell(this.coordinateX / CELL_SIZE, this.coordinateY / CELL_SIZE, true, "food", this);
     }
 
     /**
-     *
+     * Définit les coordonnées d'un fruit de sorte qu'un fruit se trouve nécessairement sur une cellule libre du terrain.
      */
     DefineCoordinates() {
         const TERRAIN = this.terrain;
+        /**
+         * @type {object} newCoordinates
+         * Entrepose les nouvelles coordonnées du fruit sous la forme d'un objet.
+         */
         let newCoordinates;
+        /**
+         * @type {object} terrainCellToCoordinates
+         * Entrepose les données d'une cellule d'une certaine position sous la forme d'un objet.
+         */
         let terrainCellToCoordinates;
 
         do {
