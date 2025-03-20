5b657bf8e49547ae8fcd514478933436c324d3f0
Noah Boos <nboosgamemaker@gmail.com>
2025-03-20 18:33:42 +0100

    Code Review - JSDoc "Snake.js"

--- src/js/class/Snake/Snake.js
+++ src/js/class/Snake/Snake.js
@@ -16,29 +16,39 @@ import {
 export class Snake {
     /**
      * Le constructeur de la classe Snake.
-     * @param document - Document du DOM.
-     * @param context - Contexte utilisé pour interagir sur le canvas.
-     * @param player - Instance de la classe PlayerAccount.
-     * @param terrain - Instance de la classe Terrain.
-     * @param coordinateX - Coordonnée X de la tête du serpent.
-     * @param coordinateY - Coordonnée Y de la tête du serpent.
-     * @param color - Couleur de la tête du serpent.
+     * @param {Document} document - Document du DOM.
+     * @param {CanvasRenderingContext2D} context - Contexte utilisé pour interagir sur le canvas.
+     * @param {PlayerAccount} player - Instance de la classe PlayerAccount.
+     * @param {Terrain} terrain - Instance de la classe Terrain.
+     * @param {number} coordinateX - Coordonnée X de la tête du serpent.
+     * @param {number} coordinateY - Coordonnée Y de la tête du serpent.
+     * @param {string} color - Couleur de la tête du serpent.
      * @constructor
      */
     constructor(document, context, player, terrain, coordinateX, coordinateY, color) {
+        /**
+         * Assignation des objets et des variables externes.
+         */
         this.document = document;
         this.context = context;
         this.player = player;
         this.terrain = terrain;
+        /**
+         * Attributs liés à la position et à la couleur du serpent en lui-même.
+         */
         this.coordinateX = coordinateX;
         this.coordinateY = coordinateY;
         this.color = color;
-
+        /**
+         *  Attributs liés au corps du serpent (Tête et Array contenant les segments du serpent.)
+         */
         this.head = new Segment(this.context, this.coordinateX, this.coordinateY, this.color);
         this.segments = new Array;
         this.segments.push(this.head);
         this.segments[0].DrawSegment();
-
+        /**
+         * Ajout d'un event listener sur le DOM permettant de récupérer des inputs et déclencher le mouvement du serpent.
+         */
         this.document.addEventListener("keydown", (event) => {
             switch (event.key) {
                 case 'z':
@@ -59,8 +69,8 @@ export class Snake {
 
     /**
      * Permet de mouvoir les segments du snake. Le comportement diffère selon si le segment à mouvoir est la tête ou une partie du corps.
-     * @param newHeadCoordinateX - Nouvelle coordonnée X de la tête.
-     * @param newHeadCoordinateY - Nouvelle coordonnée Y de la tête.
+     * @param {number} newHeadCoordinateX - Nouvelle coordonnée X de la tête.
+     * @param {number} newHeadCoordinateY - Nouvelle coordonnée Y de la tête.
      */
     MoveSnakeSegments(newHeadCoordinateX, newHeadCoordinateY) {
         if (IsThereSegmentAt(this.segments, newHeadCoordinateX, newHeadCoordinateY)) {
@@ -68,32 +78,64 @@ export class Snake {
         } else {
             for (let i = this.segments.length - 1; i >= 0; i--) {
                 if (i === 0) {
-                    // Voir "Code Review #1 - Snake" avec GPT.
+                    /**
+                     * Permet de redessiner une cellule de terrain si la tête du serpent est son seul segment.
+                     */
                     if (this.segments.length === 1) {
                         this.terrain.DrawTerrainCell(this.segments[i].coordinateX, this.segments[i].coordinateY);
                     }
-
-                    // En lien avec miamer
+                    /**
+                     * @type {Object} newPositionCellData
+                     * Objet contenant les données de la cellule de terrain où la tête va se déplacer.
+                     */
                     let newPositionCellData = this.terrain.ReadTerrainCell(newHeadCoordinateX / CELL_SIZE, newHeadCoordinateY / CELL_SIZE);
-                    // console.log(newPositionCellData);
+                    /**
+                     * Si cette cellule est de type "food", alors on effectue les actions suivantes :
+                     * - On appelle AddSegment() avec comme paramètre la valeur "segmentsToAdd" de la nourriture consommée.
+                     * - On appelle GainExp() avec comme paramètre la valeur "experience" de la nourriture consommée.
+                     * - On crée une nouvelle nourriture pour le serpent.
+                     */
                     if (newPositionCellData.type === "food") {
                         this.AddSegment(newPositionCellData.object.segmentsToAdd);
                         this.player.GainExp(newPositionCellData.object.experience);
                         CreateRandomFood(this.context, this.terrain);
                     }
-
+                    /**
+                     * Redessine la tête du serpent à sa nouvelle position.
+                     * Redéfinit également les données de cette cellule :
+                     * - isOccupied = true ;
+                     * - type = "head" ;
+                     * - object = this
+                     */
                     this.segments[i].MoveSegment(newHeadCoordinateX, newHeadCoordinateY);
                     this.terrain.WriteTerrainCell(newHeadCoordinateX / CELL_SIZE, newHeadCoordinateY / CELL_SIZE, true, "head");
                 } else {
+                    /**
+                     * Définit les coordonnées d'un n comme les coordonnées du segment n-1.
+                     * C'est-à-dire qu'un segment aura pour prochaine coordonnée les coordonnées du segment devant lui.
+                     */
                     let newCoordinateX = this.segments[i-1].coordinateX;
                     let newCoordinateY = this.segments[i-1].coordinateY;
-
+                    /**
+                     * Redessine une cellule de terrain à la précédente position du dernier segment.
+                     * Réinitialise également les données de cette cellule :
+                     * - isOccupied = false ;
+                     * - type = "ground" ;
+                     * - object = null
+                     */
                     if (i === this.segments.length - 1) {
                         this.terrain.DrawTerrainCell(this.segments[i].coordinateX, this.segments[i].coordinateY);
-                        this.terrain.WriteTerrainCell(this.segments[i].coordinateX / CELL_SIZE, this.segments[i].coordinateY / CELL_SIZE, false, "ground");
+                        this.terrain.WriteTerrainCell(this.segments[i].coordinateX / CELL_SIZE, this.segments[i].coordinateY / CELL_SIZE, false, "ground", null);
                     }
+                    /**
+                     * Redessine un segment à sa nouvelle position (à savoir l'ancienne position de la cellule avant elle dans segments[]).
+                     * Redéfinit également les données de cette cellule :
+                     * - isOccupied = true ;
+                     * - type = "segment" ;
+                     * - object = this
+                     */
                     this.segments[i].MoveSegment(newCoordinateX, newCoordinateY);
-                    this.terrain.WriteTerrainCell(newCoordinateX / CELL_SIZE, newCoordinateY / CELL_SIZE, true, "segment");
+                    this.terrain.WriteTerrainCell(newCoordinateX / CELL_SIZE, newCoordinateY / CELL_SIZE, true, "segment", this);
                 }
             }
         }
@@ -101,10 +143,18 @@ export class Snake {
 
     /**
      * Calcule les coordonnées de la tête du serpent (et donc sa direction), avant de lancer MoveSnakeSegments() pour mouvoir le serpent dans son entièreté.
-     * @param direction - Variable définissant la direction du mouvement. 0 = Avant ; 1 = Arrière ; 2 = Gauche ; 3 = Droite.
+     * @param {number} direction - Variable définissant la direction du mouvement. 0 = Avant ; 1 = Arrière ; 2 = Gauche ; 3 = Droite.
      */
     MoveSnake(direction) {
+        /**
+         * @type {number} newHeadCoordinateX
+         * Nouvelle coordonnée X de la tête du serpent.
+         */
         let newHeadCoordinateX = 0;
+        /**
+         * @type {number} newHeadCoordinateY
+         * Nouvelle coordonnée Y de la tête du serpent.
+         */
         let newHeadCoordinateY = 0;
         switch (direction) {
             case 0:
@@ -132,7 +182,7 @@ export class Snake {
 
     /**
      * Ajoute autant de segments que passés en paramètre au serpent.
-     * @param segmentsToAdd - Nombre de segments à ajouter.
+     * @param {number} segmentsToAdd - Nombre de segments à ajouter.
      */
     AddSegment(segmentsToAdd) {
         for (let i = 0; i < segmentsToAdd; i++) {
